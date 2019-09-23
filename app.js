78d2a4d2c58f3a9db7f90596b114539cded3fac9
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2019-09-23 15:03:45 -0500

    nodejs

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ socket.on('typing', function (data) {
     var message = "Welcome to Socket.IO Chat – ";
     console.log(message, data);
     const spawn = require("child_process").spawn;
-    const pythonProcess = spawn('python',["PRINT.py Print.py --firstName=Jefferson --lastName=Aguilar --company=alun ideas --assistentType=vip --assistentId=asdasdasdasdasdasdas"]);
+    const pythonProcess = spawn('python',["Print.py --firstName=Jefferson --lastName=Aguilar --company=alun ideas --assistentType=vip --assistentId=asdasdasdasdasdasdas"]);
 });
 
 socket.on('disconnect', function(){}); 
